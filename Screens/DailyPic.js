fe8c97aa04d1cec2c1a841b3a2c45c5912816c4f
Mitraa13 <mitraasrinivasan@gmail.com>
2021-06-17 18:39:51 +0530

    Proc79

--- Screens/DailyPic.js
+++ Screens/DailyPic.js
@@ -1,49 +1,138 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet,  StatusBar, Platform,SafeAreaView, ImageBackground } from 'react-native';
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    ImageBackground,
+    Alert,
+    Image,
+    ScrollView,
+    TouchableOpacity,
+    Linking
+} from "react-native";
+import axios from "axios";
 
 export default class DailyPicScreens extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            apod: {},
+        };
+    }
+
+    componentDidMount() {
+        this.getPictures()
+    }
+
+    getPictures = () => {
+        axios
+            .get("https://api.nasa.gov/planetary/apod?api_key=aQlHZPoPaPbOlq3AT6tkHG6qk9eiNIGBb7RaiDPz")
+            .then(response => {
+                this.setState({ apod: response.data })
+            })
+            .catch(error => {
+                Alert.alert(error.message)
+            })
+    }
+
+
     render() {
-        return (
-            <View style={styles.container}>
-            <SafeAreaView style={styles.androidSafeArea} />
-
-                <ImageBackground 
-                source={require('../assets/space.gif')}
-                style={styles.bgimg}
-                >
-                <Text style={styles.headerText}>DAILY PICTURES</Text>
-
-                <Text style={styles.text}>{"Sorry :( The content is yet to be updated. Check back again later!"}</Text>
-                </ImageBackground>
-                
-            </View>
-        )
+        if (Object.keys(this.state.apod).length === 0) {
+            return (
+                <View
+                    style={{flex:1, alignItems:"center", justifyContent:"center"}}>
+                    <Text style={styles.loadingText}>Loading...</Text>
+                </View>
+            )
+        } else {
+            return (
+                <View style={styles.container}>
+                    <ScrollView style={styles.scrollView}>
+                    <SafeAreaView style={styles.droidSafeArea} />
+                    <ImageBackground
+                    source={require('../assets/stars.gif')}
+                    style={styles.backgroundImage}>
+                        <View style={styles.titleBar}>
+                        <Text style={styles.titleText}>DAILY PICTURES</Text>
+                        </View>
+                        <View>
+                            <Text style={styles.dateText}>{this.state.apod.date}</Text>
+                            <TouchableOpacity
+                                style={{borderColor:"black"}}
+                                onPress={() => Linking.openURL(this.state.apod.url).catch(err => console.error("Couldn't Load Page", err))}
+                            >
+                            <Image 
+                                source={{uri:this.state.apod.hdurl}}
+                                style={{height:200, width:200, alignSelf:"center", marginTop:10}}
+                            />
+                            </TouchableOpacity>
+                            <Text style={styles.cardTitle}>{this.state.apod.title.toUpperCase()}</Text>
+                            <Text style={styles.cardText}>{this.state.apod.explanation}</Text>
+                            <Text style={styles.copyRightText}>Copy Right- {this.state.apod.copyright}</Text>
+                        </View>
+                    </ImageBackground>
+                    </ScrollView>
+                </View>
+            );
+        }
     }
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
+        flex: 1
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
         flex: 1,
+        resizeMode: 'cover',
     },
-    headerText:{
-        fontSize:50,
+    titleBar: {
+        flex: 0.15,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop:20,
+    },
+    titleText: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "white",
+        textAlign:"center"
+    },
+    cardTitle: {
+        fontSize: 25,
+        marginBottom: 10,
+        fontWeight: "bold",
+        color: "#FA00FB",
+        textAlign:"center",
+        marginTop:10,
+    },
+    cardText: {
+        color: "white",
+        margin:10,
+        fontSize:20,
+    },
+    dateText:{
+        textAlign:"right",
+        color:"white",
         fontWeight:"bold",
+        fontSize:20,
+        marginTop:20,
+        marginRight:5,
+    },
+    copyRightText:{
+        textAlign:"right",
         color:"white",
-        textAlign:"center",
+        fontWeight:"bold",
+        fontSize:20,
+        marginRight:5,
     },
-    bgimg:{
-        flex:1,
-        resizeMode:'cover',
-        paddingBottom:50
-    },
-    androidSafeArea:{
-        marginTop:Platform.OS==='android'?StatusBar.currentHeight:0
-    },
-    text:{
-        fontSize:30,
-        color:"black",
-        textAlign:"center",
-        color:"white",
-        marginTop:100
+    scrollView:{
+        marginHorizontal:1,
     }
-})
+});
